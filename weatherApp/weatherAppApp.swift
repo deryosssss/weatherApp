3d560364a96d2debf61b6ad12e0d1ec1f062b234
derya <derya-baglan@hotmail.com>
2025-03-05 02:08:39 +0000

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Derya Baglan on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
